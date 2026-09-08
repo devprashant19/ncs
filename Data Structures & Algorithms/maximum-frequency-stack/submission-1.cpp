e7e0fd30c60ec38403ca6938ccf124ce11e25cb9
Prashant <dev.prashant1901@gmail.com>
2026-09-08 19:00:27 +0530

    Add: maximum-frequency-stack - submission-1

---
+++ Data Structures & Algorithms/maximum-frequency-stack/submission-1.cpp
@@ -0,0 +1,20 @@
+class FreqStack {
+private:
+    priority_queue<vector<int>> heap;
+    unordered_map<int, int> cnt;
+    int index;
+
+public:
+    FreqStack() : index(0) {}
+    void push(int val) {
+        cnt[val]++;
+        heap.push({cnt[val], index++, val});
+    }
+    int pop() {
+        auto top = heap.top();
+        heap.pop();
+        int val = top[2];
+        cnt[val]--;
+        return val;
+    }
+};
